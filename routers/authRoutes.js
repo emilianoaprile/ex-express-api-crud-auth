337c6cc94c43b5d8d17d0738d5bae0fb42ea3566
EmilianoAprile <emilianoaprile77@icloud.com>
2024-06-10 16:19:27 +0200

    add > auth routes & user body register validations

--- routers/authRoutes.js
+++ routers/authRoutes.js
@@ -2,10 +2,10 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController.js');
-// const validator = require('../middlewares/validatorFunction.js');
-// const {bodyRegister, bodyLogin} = require('../validations/userValidation.js');
+const validator = require('../middlewares/validatorFunction.js');
+const {registerBody} = require('../validations/usersValidation.js');
 
-router.post('/register', authController.register)
+router.post('/register', validator(registerBody), authController.register)
 // router.post('/login', authController.login)
 
 module.exports = router;
--- validations/usersValidation.js
+++ validations/usersValidation.js
@@ -1,1 +1,61 @@
-// validazione dei dati in ingresso dell'utente
+const {PrismaClient} = require("@prisma/client")
+const prisma = new PrismaClient();
+
+const registerBody = {
+    email: {
+        in: ['body'],
+        notEmpty: {
+            errorMessage: 'L\'email è obbligatoria',
+            bail: true
+        },
+        isEmail: {
+            errorMessage: 'L\'email non è valida',
+            bail: true
+        },
+        custom: {
+            options: async (value) => {
+                const user = await prisma.user.findUnique({
+                    where: {
+                        email: value
+                    }
+                })
+                if (user) {
+                    throw new Error('L\'email è già in uso')
+                }
+
+                return true
+            }
+        }
+    },
+    name: {
+        in: ['body'],
+        isString: {
+            errorMessage: 'Il nome deve essere una stringa',
+            bail: true
+        },
+        isLength: {
+            errorMessage: 'Il nome deve essere di almeno 3 caratteri',
+            options: {min: 3}
+        }
+    },
+    password: {
+        in: ['body'],
+        notEmpty: {
+            errorMessage: 'La password è obbligatoria',
+            bail: true
+        },
+        isString: {
+            errorMessage: 'La password deve essere una stringa',
+            bail: true
+        },
+        isLength: {
+            errorMessage: 'La password deve essere di almeno 8 caratteri',
+            options: {min: 8}
+        }
+    }
+}
+
+
+module.exports = {
+    registerBody
+}
